35db40a06ca263f1319d405935c7c0e80c56744f
Rom Grk <romgrk.cc@gmail.com>
2020-07-27 16:58:07 -0400

    image: crop to square before saving

--- client/src/helpers/resize-image.js
+++ client/src/helpers/resize-image.js
@@ -11,28 +11,36 @@ export default function resizeImage(file, maxSize){
     // Load the image
     const reader = new FileReader()
     reader.onload = function (readerEvent) {
+
       const image = new Image()
       image.onload = function (imageEvent) {
+
         // Resize the image
         const canvas = document.createElement('canvas')
 
+        let x = 0
+        let y = 0
         let width  = image.width
         let height = image.height
 
         if (width > height) {
           if (width > maxSize) {
-            height *= maxSize / width;
-            width = maxSize;
-          }
-        } else {
-          if (height > maxSize) {
             width *= maxSize / height;
             height = maxSize;
           }
+          const diff = width - height
+          x = -diff / 2
+        } else {
+          if (height > maxSize) {
+            height *= maxSize / width;
+            width = maxSize;
+          }
+          const diff = height - width
+          y = -diff / 2
         }
-        canvas.width = width;
-        canvas.height = height;
-        canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+        canvas.width  = maxSize
+        canvas.height = maxSize
+        canvas.getContext('2d').drawImage(image, x, y, width, height)
 
         const dataUrl = canvas.toDataURL('image/png')
 
