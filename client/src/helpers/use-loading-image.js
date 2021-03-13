8be082333ec1aa151476c3f8e95352c4f43459cd
Rom Grk <romgrk.cc@gmail.com>
2021-03-12 23:17:21 -0500

    fix(useLoadingImage): better behavior

--- client/src/helpers/use-loading-image.js
+++ client/src/helpers/use-loading-image.js
@@ -7,29 +7,37 @@ import { useState, useEffect } from 'react'
 const loadedImages = {}
 
 function loadImage(source) {
-  return new Promise((resolve, reject) => {
+  if (loadedImages[source])
+    return loadedImages[source]
+
+  const loader = new Promise((resolve, reject) => {
     // timeout to allow data more time to load
     setTimeout(() => {
       const imgLoader = new Image()
       imgLoader.onload = function(){
         resolve(this.src)
+        loader.done = true
       }
       imgLoader.src = source
     }, 250)
   })
+  loadedImages[source] = loader
+  return loader
 }
 
 export default function useLoadingImage(source) {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(
+    source in loadedImages ?
+      loadedImages[source].done !== true :
+      true
+  )
 
   useEffect(() => {
     if (!source)
       return
-    const loader = loadedImages[source] || loadImage(source)
-    loader.then(() => {
+    loadImage(source).then(() => {
       setIsLoading(false)
     })
-    loadedImages[source] = loader
   }, [source])
 
   return isLoading ? null : source
